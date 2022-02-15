5c2b23a1d8865a5a0159145e46b473688fd98708
somsomdah <somdah98@gmail.com>
2022-02-15 22:25:49 +0900

    [themes] Add themes

---
+++ src/themes/colors.js
@@ -0,0 +1,13 @@
+export const color = {
+    primary: '#70A0FF',
+    primaryLight: '#F0F5FF',
+    primaryDark: '#5F8DE8',
+    gray2: '#F8F8F8',
+    gray3: '#EEEEEE',
+    gray4: '#D7D7D7',
+    gray6: '#4B4B4B',
+    gray7: '#848484',
+    gray8: {color: '#515151', opacity: 0.2},
+    black: '#1B1B1B',
+    white: '#FEFEFE'
+};
---
+++ src/themes/images.js
@@ -0,0 +1,38 @@
+// import * as icons from '../assets/icons/';'
+
+export const image = {
+    bottom: {
+        unfocused: {
+            alarm: require('../../assets/icons/alarm_gray4.png'),
+            category: require('../../assets/icons/category_gray4.png'),
+            home: require('../../assets/icons/home_gray4.png'),
+            settings: require('../../assets/icons/settings_gray4.png'),
+        },
+
+        focused: {
+            alarm: require('../../assets/icons/alarm_primary.png'),
+            category: require('../../assets/icons/category_primary.png'),
+            home: require('../../assets/icons/home_primary.png'),
+            settings: require('../../assets/icons/settings_primary.png'),
+        }
+    },
+    star: {
+        filled: require('../../assets/icons/star_filled.png'),
+        unfilled: require('../../assets/icons/star_unfilled.png')
+    },
+    common: {
+        return: require('../../assets/icons/return.png'),
+        search: {
+            black: require('../../assets/icons/search_black.png'),
+            primary: require('../../assets/icons/search.png')
+        },
+        delete: require('../../assets/icons/delete.png')
+    },
+
+    school: {
+        ewha: require('../../assets/schools/ewha-logo.png'),
+        hongik: require('../../assets/schools/hongik-logo.png'),
+        seogang: require('../../assets/schools/seogang-logo.png'),
+
+    }
+}
