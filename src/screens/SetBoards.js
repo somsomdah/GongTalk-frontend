a6af770167f44c0b0fd19fdfccc7f254bdfc13b2
somsomdah <somdah98@gmail.com>
2022-02-18 17:55:53 +0900

    게시판 순서 바꿀때 뒤에 배경 보이게

--- src/screens/SetBoards.js
+++ src/screens/SetBoards.js
@@ -18,6 +18,18 @@ const data = [
     { id: 2, name: '서강대학교 컴퓨터공학과' },
     { id: 3, name: '연세대학교 컴퓨터공학과' },
     { id: 4, name: '이화여자대학교 엘텍공과대학' },
+    { id: 5, name: '이화여자대학교 컴퓨터공학과' },
+    { id: 6, name: '서강대학교 컴퓨터공학과' },
+    { id: 7, name: '연세대학교 컴퓨터공학과' },
+    { id: 8, name: '이화여자대학교 엘텍공과대학' },
+    { id: 9, name: '이화여자대학교 컴퓨터공학과' },
+    { id: 10, name: '서강대학교 컴퓨터공학과' },
+    { id: 11, name: '연세대학교 컴퓨터공학과' },
+    { id: 12, name: '이화여자대학교 엘텍공과대학' },
+    { id: 13, name: '이화여자대학교 컴퓨터공학과' },
+    { id: 14, name: '서강대학교 컴퓨터공학과' },
+    { id: 15, name: '연세대학교 컴퓨터공학과' },
+    { id: 16, name: '이화여자대학교 엘텍공과대학' },
 ]
 
 
@@ -37,7 +49,9 @@ const SetBoards = ( ) => {
                 keyExtractor={(board) => board.id}
                 data={boards}
                 onDragEnd={({ data }) => setBoards(data)}
-                renderItem={renderItem} />
+                renderItem={renderItem} 
+                containerStyle={{backgroundColor: color.primaryLight}}
+                />
         </Container>
 
     )
