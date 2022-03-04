a7144f6f88be62b396f1ec5cade4468f0b58d60f
somsomdah <somdah98@gmail.com>
2022-03-04 14:30:00 +0900

    TeamInfo 일러스트 넣음

--- src/components/teamInfo/Content.js
+++ src/components/teamInfo/Content.js
@@ -10,7 +10,7 @@ const Container = styled.View`
     justify-content: space-around;
     align-items: stretch;
     background-color: ${color.white};
-    padding: 40px;
+    padding: 40px 0px 40px 40px;
 `;
 
 const TextBox = styled.View`
@@ -32,22 +32,32 @@ const Symbol = styled.Image.attrs({
     height: 140px;
 `
 
+const Illust = styled.Image.attrs({
+    source: image.illust.teaminfo
+})`
+    width: 360px;
+    height: 360px;
+    align-self: flex-end;
+`
+
 const Content = () => {
     const _style = { color: color.primary };
+    const _style2 = {color: color.gray6, letterSpacing: 1}
     return (
         <Container>
             <Symbol />
+            <Illust />
             <TextBox>
                 <Line>
-                    <SemiHeadline2_1 style={_style}>{'Prod. Dev. '}</SemiHeadline2_1>
-                    <SemiHeadline2_1 style={{color: color.gray6}}>{'장다솜'}</SemiHeadline2_1>
+                    <SemiHeadline2_1 style={_style}>{'Prod. Dev.  '}</SemiHeadline2_1>
+                    <SemiHeadline2_1 style={_style2}>{'장다솜'}</SemiHeadline2_1>
                 </Line>
                 <Line>
-                    <SemiHeadline2_1 style={_style}>{'UX. BX. '}</SemiHeadline2_1>
-                    <SemiHeadline2_1 style={{color: color.gray6}}>{'오채윤'}</SemiHeadline2_1>
+                    <SemiHeadline2_1 style={_style}>{'UX. BX.  '}</SemiHeadline2_1>
+                    <SemiHeadline2_1 style={_style2}>{'오채윤'}</SemiHeadline2_1>
                 </Line>
                 <Line>
-                    <SemiHeadline2_1 style={_style}>{'Contact. '}</SemiHeadline2_1>
+                    <SemiHeadline2_1 style={_style}>{'Contact.  '}</SemiHeadline2_1>
                     <SemiHeadline2_1 style={{color: color.gray6}}>{'gongtalk.contact@gmail.com'}</SemiHeadline2_1>
                 </Line>
             </TextBox>
