5ef074fb27b74afac9a21dce0e9c30f1e3fd5b97
somsomdah <somdah98@gmail.com>
2022-03-08 19:06:16 +0900

    공지사항 페이지 리뉴얼

--- src/navigations/AppStack.js
+++ src/navigations/AppStack.js
@@ -2,14 +2,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Search from '../screens/Search';
 import BottomTabNavigation from './BottomTab';
 import AddBoard from '../screens/AddBoard';
-import NoticeList from '../screens/NoticeList';
-import NoticeDetail from '../screens/NoticeDetail';
 import PostList from '../screens/PostList';
 import PostDetail from '../screens/PostDetail';
 import SearchList from '../screens/SearchList';
 import TeamInfo from '../screens/TeamInfo';
 import AddKeyword from '../screens/AddKeyword';
 import SearchBoard from '../screens/SearchBoard';
+import Notice from '../screens/Notice';
 
 const Stack = createStackNavigator();
 
@@ -31,8 +30,11 @@ const AppStackNavigation = () => {
             <Stack.Screen name='postList' component={PostList} />
             <Stack.Screen name='postDetail' component={PostDetail} />
 
+            {/* Deprecated
             <Stack.Screen name='noticeList' component={NoticeList} />
-            <Stack.Screen name='noticeDetail' component={NoticeDetail} />
+            <Stack.Screen name='noticeDetail' component={NoticeDetail} /> */}
+
+            <Stack.Screen name='notice' component={Notice} />
             <Stack.Screen name='teamInfo' component={TeamInfo} />
 
         </Stack.Navigator>
