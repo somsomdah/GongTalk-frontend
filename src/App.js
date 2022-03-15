e66323c566810cff2cd480afb1852ae0ef0bbb6d
somsomdah <somdah98@gmail.com>
2022-03-15 14:14:16 +0900

    QueryClientProvider 추가

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import styled from "styled-components/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from 'react-native';
 import { useFonts } from "expo-font";
+import { useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
 import { color } from './common/colors';
 import { font } from "./common/fonts";
 import OnboardingNavigation from "./navigations/OnboardingStack";
 import AppStackNavigation from './navigations/AppStack';
 
+
 const App = () => {
 
     const [loaded, error] = useFonts(font.pretendard);
-    const Stack = createStackNavigator()
+    const Stack = createStackNavigator();
+    const queryClient = new QueryClient();
+
 
     return (
         loaded &&
-        <Container>
-            <StatusBar backgroundColor={color.white} barStyle='dark-content' />
-            <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{ headerShown: false, animationEnabled: false }}
-                initialRouteName='onboarding'
-            >
-                <Stack.Screen name={'app'} component={AppStackNavigation} />
-                <Stack.Screen name={'onboarding'} component={OnboardingNavigation} />
-            </Stack.Navigator>
-            </NavigationContainer>
-        </Container>
+        <QueryClientProvider client={queryClient}>
+            <Container>
+                <StatusBar backgroundColor={color.white} barStyle='dark-content' />
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={{ headerShown: false, animationEnabled: false }}
+                        initialRouteName='onboarding'
+                    >
+                        <Stack.Screen name={'app'} component={AppStackNavigation} />
+                        <Stack.Screen name={'onboarding'} component={OnboardingNavigation} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </Container>
+        </QueryClientProvider>
+
     );
 };
 
