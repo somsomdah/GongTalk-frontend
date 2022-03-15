a77b85441081aa51de59856ff024814aa47a65f6
somsomdah <somdah98@gmail.com>
2022-03-15 17:59:43 +0900

    ContextProvider 넣어줌

--- src/navigations/AppStack.js
+++ src/navigations/AppStack.js
@@ -9,35 +9,38 @@ import TeamInfo from '../screens/TeamInfo';
 import AddKeyword from '../screens/AddKeyword';
 import SearchBoard from '../screens/SearchBoard';
 import Notice from '../screens/Notice';
+import { UserProvider } from '../contexts/User';
 
 const Stack = createStackNavigator();
 
 const AppStackNavigation = () => {
     return (
-        <Stack.Navigator
-            screenOptions={{ headerShown: false, animationEnabled: false }}
-            initialRouteName='main'
-        >
-            <Stack.Screen name='main' component={BottomTabNavigation} />
+        <UserProvider>
+            <Stack.Navigator
+                screenOptions={{ headerShown: false, animationEnabled: false }}
+                initialRouteName='main'
+            >
+                <Stack.Screen name='main' component={BottomTabNavigation} />
 
-            <Stack.Screen name="search" component={Search} />
-            <Stack.Screen name='searchList' component={SearchList} />
+                <Stack.Screen name="search" component={Search} />
+                <Stack.Screen name='searchList' component={SearchList} />
 
-            <Stack.Screen name="addBoard" component={AddBoard} />
-            <Stack.Screen name='searchBoard' component={SearchBoard} />
-            <Stack.Screen name='addKeyword' component={AddKeyword} />
+                <Stack.Screen name="addBoard" component={AddBoard} />
+                <Stack.Screen name='searchBoard' component={SearchBoard} />
+                <Stack.Screen name='addKeyword' component={AddKeyword} />
 
-            <Stack.Screen name='postList' component={PostList} />
-            <Stack.Screen name='postDetail' component={PostDetail} />
+                <Stack.Screen name='postList' component={PostList} />
+                <Stack.Screen name='postDetail' component={PostDetail} />
 
-            {/* Deprecated
+                {/* Deprecated
             <Stack.Screen name='noticeList' component={NoticeList} />
             <Stack.Screen name='noticeDetail' component={NoticeDetail} /> */}
 
-            <Stack.Screen name='notice' component={Notice} />
-            <Stack.Screen name='teamInfo' component={TeamInfo} />
+                <Stack.Screen name='notice' component={Notice} />
+                <Stack.Screen name='teamInfo' component={TeamInfo} />
 
-        </Stack.Navigator>
+            </Stack.Navigator>
+        </UserProvider>
     );
 };
 
