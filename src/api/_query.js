b273925393d6a6c23d618c93df24f726d58242c4
somsomdah <somdah98@gmail.com>
2022-03-15 13:49:23 +0900

    쿼리 날리는 함수들

---
+++ src/api/_query.js
@@ -0,0 +1,122 @@
+import axios from 'axios'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import ENV_VARS from 'env'
+
+const baseUrl = `${ENV_VARS.apiUrl}/api/`
+
+const _get = async (url, params, authToken) => {
+    const { data, status } = await axios.get(`${baseUrl}/${url}`, {
+        params: { params },
+        headers: authToken && { 'Authorization': `Bearer ${authToken}` }
+    });
+
+    return { body: data, statusCode: status }
+};
+
+const _post = async (url, body, authToken) => {
+    const { data, status } = await axios.post(`${baseUrl}/${url}`, body, {
+        headers: authToken && { 'Authorization': `Bearer ${authToken}` },
+    })
+
+    return { body: data, statusCode: status }
+};
+
+const _patch = async (url, body, authToken) => {
+    const { data, status } = await axios.patch(`${baseUrl}/${url}`, body, {
+        headers: authToken && { 'Authorization': `Bearer ${authToken}` },
+    })
+
+    return { body: data, statusCode: status }
+};
+
+const _delete = async (url, authToken) => {
+    const { data, status } = await axios.delete(`${baseUrl}/${url}`, {
+        headers: authToken && { 'Authorization': `Bearer ${authToken}` },
+    })
+
+    return { body: data, statusCode: status }
+}
+
+
+// ================================================================================================================== //
+
+const queryClient = useQueryClient()
+
+const Exception = (method, error, url) => {
+    console.log('==========================================');
+    console.log(`[API] (${method}) ${url}`)
+    console.log(`[ERROR] ${error.message}`);
+    console.log('==========================================');
+}
+
+const GET = (keyList, url, params, authToken, options) => {
+    const { data, error, status } = useQuery(keyList, _get(url, params, authToken), options)
+
+    if (status === 'error') {
+        throw new Exception('GET', error.message, url)
+    } else {
+        console.log('==========================================')
+        console.log(`[API] (GET) ${url}`)
+        console.log(`[RESPONSE] ${data}`);
+        console.log('==========================================')
+    }
+
+    return data;
+};
+
+const POST = (url, body, authToken, invalidateKey) => {
+    const { data, error, status } = useMutation(_post(url, body, authToken), 
+    {
+        onSuccess: () => invalidateKey ? queryClient.invalidateQueries(invalidateKey) : null
+    })
+
+    if (status === 'error') {
+        throw new Exception('POST', error.message, url)
+    } else {
+        console.log('==========================================')
+        console.log(`[API] (POST) ${url}`)
+        console.log(`[RESPONSE] ${data}`);
+        console.log('==========================================')
+    }
+
+    return data;
+};
+
+const PATCH = (url, body, authToken, invalidateKey) => {
+    const { data, error, status } = useMutation(_patch(url, body, authToken),
+    {
+        onSuccess: () => invalidateKey ? queryClient.invalidateQueries(invalidateKey) : null
+    });
+
+    if (status === 'error') {
+        throw new Exception('POST', error.message, url)
+    } else {
+        console.log('==========================================')
+        console.log(`[API] (PATCH) ${url}`)
+        console.log(`[RESPONSE] ${data}`);
+        console.log('==========================================')
+    }
+
+    return data;
+};
+
+const DELETE = (url, authToken, invalidateKey) => {
+    const { data, error, status } = useMutation(_delete(url, authToken),     {
+        onSuccess: () => invalidateKey ? queryClient.invalidateQueries(invalidateKey) : null
+    });
+
+    if (status === 'error') {
+        throw new Exception('POST', error.message, url)
+    } else {
+        console.log('==========================================')
+        console.log(`[API] (DELETE) ${url}`)
+        console.log(`[RESPONSE] ${data}`);
+        console.log('==========================================')
+    }
+
+    return data;
+};
+
+export {
+    GET, POST, PATCH, DELETE
+}
