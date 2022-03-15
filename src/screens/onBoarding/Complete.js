dba170c12b6804daaf0f40c1a5e928825ae90432
somsomdah <somdah98@gmail.com>
2022-03-15 17:59:15 +0900

    api를 연결하고자 하는 시도

--- src/screens/onBoarding/Complete.js
+++ src/screens/onBoarding/Complete.js
@@ -4,6 +4,13 @@ import { image } from "../../common/images";
 import { Headline4, SemiHeadline1 } from "../../components/_common/Typography";
 import { Pressable, View } from "react-native";
 import { ReturnButton, NextButton, ButtonContainer } from "./Start";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useContext } from "react";
+import { useMutation } from 'react-query';
+import { createUser, login } from "api/user";
+import { getUniqueId } from "react-native-device-info";
+import OnboardingContext from "../../contexts/Onboarding";
+import UserContext from "../../contexts/User";
 
 const Container = styled.View`
     flex: 1;
@@ -29,25 +36,63 @@ const CheckImage = styled.Image.attrs({
 `
 const Complete = ({ navigation }) => {
 
-    return (
-        <Container>
-            <View style={{ alignItems: 'center' }}>
-                <View style={{height: 52}}/>
-                <Illust />
-                <View style={{height: 120}}/>
-                <Headline4 style={{ color: color.primary}}>{'모든 설정을 완료했어요!'}</Headline4>
-                <View style={{height: 16}}/>
-                <SemiHeadline1 style={{ color: color.gray6 }}>
-                    {'이제 공똑을 시작해볼까요?'}
-                </SemiHeadline1>
-            </View>
-            <Space />
-            <ButtonContainer>
-                <ReturnButton value={'이전'} onPress={() => navigation.navigate('onboarding-addKeyword')} />
-                <NextButton value={'시작하기'} onPress={() => navigation.navigate('app')} />
-            </ButtonContainer>
-        </Container>
-    );
+    const { boardList, keywordList } = useContext(OnboardingContext);
+    const deviceNum = getUniqueId();
+
+    const createUser = useMutation(createUser);
+    const login = useMutation(login);
+    // board랑 keyword user와 함께 저장함
+
+    createUser.mutate({ deviceNum: deviceNum }, {
+        onSuccess: () => {
+            login.mutate({ deviceNum: deviceNum },
+                {
+                    onSuccess: () => {
+                        // board랑 keyword user와 함께 저장함
+                    }
+                }
+            )
+        }
+    });
+
+    // const userCheck = new Promise((resolve, reject) => {
+    //     createUser.mutate({ deviceNum: deviceNum }, {onSuccess : () => {
+    //         login.mutate({deviceNum: deviceNum},
+    //             {onSuccess : (data) => {
+    //                 resolve(data)
+    //             }}
+    //             )
+    //     }});
+
+    // })
+
+    // userCheck().then(result => {})
+
+
+
+    navigation.navigate('app')
+}
+
+
+return (
+    <Container>
+        <View style={{ alignItems: 'center' }}>
+            <View style={{ height: 52 }} />
+            <Illust />
+            <View style={{ height: 120 }} />
+            <Headline4 style={{ color: color.primary }}>{'모든 설정을 완료했어요!'}</Headline4>
+            <View style={{ height: 16 }} />
+            <SemiHeadline1 style={{ color: color.gray6 }}>
+                {'이제 공똑을 시작해볼까요?'}
+            </SemiHeadline1>
+        </View>
+        <Space />
+        <ButtonContainer>
+            <ReturnButton value={'이전'} onPress={() => navigation.navigate('onboarding-addKeyword')} />
+            <NextButton value={'시작하기'} onPress={onStartButtonPress} />
+        </ButtonContainer>
+    </Container>
+);
 }
 
 export default Complete;
